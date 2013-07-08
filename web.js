dd7de23fcef9e5e7fcb5b769e6165e01ede9d34f
KK Gian <kkgian@gmail.com>
2013-07-08 15:23:18 +0800

    web.js now read frim index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	  fs.readFile('index.html', function (err, data) {
+	    if (err) throw err;
+		response.end(data);
+	  });
 });
 
 var port = process.env.PORT || 5000;
